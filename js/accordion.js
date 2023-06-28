c603229dad733acc04661c42386ca39b1d139178
Thomas Karam <131653869+SkulduggeryDude@users.noreply.github.com>
2023-06-29 08:33:39 +1200

    Uploaded files

---
+++ js/accordion.js
@@ -0,0 +1,16 @@
+//Javascript for accordion, taken from W3Schools. Don't edit
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
